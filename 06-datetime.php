d0fc0eb6fde1efd8fe3c6a1f4a41f3d33e136364
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-09-03 12:50:34 +0200

    valider par le formateur

--- 06-datetime.php
+++ 06-datetime.php
@@ -1,6 +1,9 @@
 <?php
+
+use LDAP\Result;
+
 require "./function.php";
-function getToday():string
+function getToday(): string
 {
     $date = new DateTime();
     $today = $date->format("d/m/y");
@@ -12,16 +15,28 @@ function getTimeLeft(string $eventDate)
     $date = new DateTime($eventDate);
     $today = new DateTime();
     $interval = $today->diff($date);
-    if ($interval->invert == 1 && $interval->days == 0){
+    if ($interval->invert == 1 && $interval->days == 0) {
         return "Aujourd'hui";
-    }elseif($interval->invert == 1 && $interval->days != 0){
+    } elseif ($interval->invert == 1 && $interval->days != 0) {
         return "Évènement passé";
-    }else{
-        
+    } else {
+        $result = "Dans ";
+        if ($interval->y != 0) {
+            $result .= $interval->y == 1 ? $interval->y . " an " : $interval->y . " ans ";
+        }
+        if ($interval->m > 0) {
+            $result .= $interval->d == 0 && $interval->y >= 0 ? "et " . $interval->y . "mois " : $interval->m . " mois ";
+        }
+        if ($interval->d > 0) {
+            $result .= $interval->y > 0 || $interval->m > 0 ? "et " : "";
+            $result .= $interval->d > 1 ? $interval->d . " jours " : $interval->d . " jour ";
+        }
+        return $interval;
+
     }
 }
 
 
 
 getToday();
-var_dump(getTimeLeft("02-10-2024"));
+var_dump(getTimeLeft("03-09-2024"));
