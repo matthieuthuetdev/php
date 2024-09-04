ce8b7bd617c599db0ec953253a37abf184885aed
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-09-04 09:41:08 +0200

    draft

--- draft.php
+++ draft.php
@@ -1,25 +1,5 @@
 <?php
-function getTimeLeft(string $userDate): string
-{
-    $myUserDate = new datetime("$userDate");
-    $test = $myUserDate->format("Y-m-d");
-    echo $test;
-    $date = new datetime("now");
-
-    if ($myUserDate < $date) {
-        return "Évènement passé";
-    } elseif ($myUserDate === $date) {
-        return "Aujourd'hui";
-    } else {
-        return "Évenement futur";
-    }
-}
-
-
-
-
-echo getTimeLeft('2022-04-22');
-echo PHP_EOL;
-echo getTimeLeft('2024-09-03');
-echo PHP_EOL;
-echo getTimeLeft('2025-04-22');
+$date1 = new DateTime();
+$date2 = new DateTime("07/11/2031");
+$interval = $date1->diff($date2);
+echo "aujourd'huit j'ai". $interval->y."ans ". $interval->m . "mois";
