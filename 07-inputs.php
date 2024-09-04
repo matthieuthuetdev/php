ed9839259748e897ff31e58b8e044f3af20b5800
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-09-04 12:54:19 +0200

    f

---
+++ 07-inputs.php
@@ -0,0 +1,2 @@
+<?php
+function stringLength (string $)motDePasse
--- draft.php
+++ draft.php
@@ -1,5 +1,3 @@
 <?php
-$date1 = new DateTime();
-$date2 = new DateTime("07/11/2031");
-$interval = $date1->diff($date2);
-echo "aujourd'huit j'ai". $interval->y."ans ". $interval->m . "mois";
+$interval = date_diff("1/01/2023",05/15/2010);
+var_dump($interval);
