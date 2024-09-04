c28a778c94412fa9e35ebcbb0a495c7802a317f3
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-09-04 15:59:26 +0200

    f

--- 07-inputs.php
+++ 07-inputs.php
@@ -1,2 +1,15 @@
 <?php
-function stringLength (string $)motDePasse
+function stringLength(string $password): bool
+{
+    $passwordClean = str_replace(" ", "", $password);
+    $passwordSize = strlen($passwordClean);
+    if ($passwordSize >= 9) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+var_dump(stringLength("mmj"));
